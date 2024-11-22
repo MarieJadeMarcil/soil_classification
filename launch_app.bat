745ff9a55828f23d64d18174322293f880b0d29d
MarieJadeMarcil <marie-jade.marcil@polymtl.ca>
2024-11-22 16:03:13 -0600

    Add Python Scripts to Launch Application

---
+++ launch_app.bat
@@ -0,0 +1,7 @@
+@echo off
+
+:: Install dependencies
+pip install -r requirements.txt
+
+:: Launch the application
+python app.py
---
+++ launch_app.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Launch the application
+python3 app.py
